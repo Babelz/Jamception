77fc838f96665901dc39e2b44b51fc98b01b0d22
eeneku <eeneku@hotmail.com>
2015-01-23 23:42:03 +0200

    1D update

---
+++ Assets/Scripts/Computer.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Computer : MonoBehaviour {
+    public float minTime = 0.5f;
+    public float maxTime = 5f;
+
+    private float timer;
+    private int state;
+    private enum States { Normal, Bugged, BSOD };
+
+	// Use this for initialization
+	void Start () {
+        state = (int)States.Normal;
+        timer = Random.Range(minTime, maxTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+
+        if (timer < 0 && state == (int)States.Normal)
+        {
+            gameObject.renderer.material.color = Color.green;
+            state = (int)States.Bugged;
+            timer = 3;
+        }
+        else if (timer < 0 && state == (int)States.Bugged)
+        {
+            gameObject.renderer.material.color = Color.blue;
+            state = (int)States.BSOD;
+            timer = 10;
+        }
+        else if (timer < 0 && state == (int)States.BSOD)
+        {
+            gameObject.renderer.material.color = Color.white;
+            state = (int)States.Normal;
+            timer = Random.Range(minTime, maxTime);
+        }
+	}
+
+    public bool IsBugged()
+    {
+        return (state == (int)States.Bugged);
+    }
+
+    public void Fix()
+    {
+        gameObject.renderer.material.color = Color.white;
+        state = (int)States.Normal;
+        timer = Random.Range(minTime, maxTime);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 0.1f;
+    public GameObject[] computers;
+
+	// Use this for initialization
+	void Start () {
+        computers = GameObject.FindGameObjectsWithTag("Computer");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.right * Input.GetAxis("Horizontal") * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+	        foreach (GameObject computer in computers)
+            {
+                Vector2 heading = computer.transform.position - transform.position;
+                float distance = heading.magnitude;
+                Vector2 direction = heading / distance;
+
+                if (heading.sqrMagnitude < 0.25 && computer.GetComponent<Computer>().IsBugged())
+                {
+                    computer.renderer.material.color = Color.gray;
+                    computer.GetComponent<Computer>().Fix();
+                }
+            }
+        }
+	}
+}

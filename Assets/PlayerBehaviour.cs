1520bae1f0670e5479fd7961b46bb7b005f7f9fb
unknown <ttv14sjanin@TA11L155-30.opetus.kajak.fi>
2015-01-24 00:09:17 +0200

    Created tile tests

---
+++ Assets/PlayerBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    public float Speed = 5f;
+	// Use this for initialization
+	void Start () {
+        Physics2D.gravity = new Vector3(0f, 0f, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        transform.position += transform.right * Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+        transform.position += transform.up * Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+    }
+}

cab244cb874d63003d3ee29c166eeeda0458399d
siquel <squual@kahvipaussi.net>
2015-01-25 02:30:10 +0200

    Unity is awesome, rewrite ALL the things!

--- Assets/TableBehaviour.cs
+++ Assets/TableBehaviour.cs
@@ -2,44 +2,38 @@
 using System.Collections.Generic;
 
 public class TableBehaviour : MonoBehaviour {
+    private static System.Random random = new System.Random();
 
+    public bool SpawnPizza = false;
+    public GameObject PizzaBox;
 
-    public int InitialPizzaCount = 3;
-
-    private List<GameObject> pizzas =  new List<GameObject>();
-
-    public GameObject Pizza;
+    // pizza box 
+    private GameObject box;
 
     public bool HasPizza
     {
-        get
-        {
-            return pizzas.Count > 0;
-        }
+        get { return box != null; }
     }
+
+    public PizzaSpawner PizzaBehaviour
+    {
+        get { return box.GetComponent<PizzaSpawner>();  }
+    }
+
 	// Use this for initialization
 	void Start () {
-        
-        for (int i = 0; i < InitialPizzaCount; i++)
-        {
-            GameObject pizza = GameObject.Instantiate(Pizza) as GameObject;
-            Vector3 pos = this.transform.position;
-            pos.z -= 3f;
-            pos.x += i * 0.45f;
-            pizza.transform.position = pos;
-            pizzas.Add(pizza);
-            
-        }
-	}
-	
-    public void GimmePizza()
-    {
-        if (pizzas.Count == 0) return;
 
-        GameObject pizza = pizzas[pizzas.Count - 1];
-        pizzas.Remove(pizza);
-        Destroy(pizza);
-    }
+        if (!SpawnPizza) return;
+
+        var container = transform.GetChild(0);
+        box = Instantiate(PizzaBox) as GameObject;
+        box.transform.parent = container;
+        box.transform.position = container.transform.position;
+
+        var spawner = box.AddComponent<PizzaSpawner>();
+        spawner.Spawn(box, "Pizza", random.Next(1,3));
+
+	}
 
 	// Update is called once per frame
 	void Update () {
